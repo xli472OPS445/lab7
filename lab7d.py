aa9d7ee8266a7be25c2fae9ddc5bc85c30330c8e
xli472OPS445 <lavylabonte@gmail.com>
2024-11-19 16:17:47 -0500

    finish lab7d

--- lab7d.py
+++ lab7d.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id] 
+# Student ID: [xli472] 
 class Time:
     """Simple object type for time of the day.
         data attributes: hour, minute, second
